57deedc66ee3f57fd854ff9c6bf28f23d39f7ec9
sofia <sormazabal@uc.cl>
2015-09-06 23:27:11 -0300

    Lo que alcanc'e despu'es de pelear con el router
    Se me cay[o la internet y tuve que subir

--- mapa.js
+++ mapa.js
@@ -1,16 +1,6 @@
 // See post: http://asmaloney.com/2014/01/code/creating-an-interactive-map-with-leaflet-and-openstreetmap/
-/*
-var map = L.map( 'map', {
-    center: [20.0, 5.0],
-    minZoom: 2,
-    zoom: 2
-});*/
-
-/*L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-    subdomains: ['otile1','otile2','otile3','otile4']
-}).addTo( map );
-*/
+Fuentes:
+Tutorial de Leaflet
 L.mapbox.accessToken = 'pk.eyJ1Ijoic29ybWF6YWJhbCIsImEiOiJkZTk1MzE1MmI2Mjk0YTg4MjUwMWY5NDlmYjk3ODkzMSJ9.X8cHVGNC_Onq0IdRV8jWVw';
 // Create a map in the div #map
 var map =L.mapbox.map('map', 'sormazabal.ff680016');
